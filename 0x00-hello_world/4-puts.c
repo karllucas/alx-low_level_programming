5c5658664e3c1916aeb2d8c50782dc2cda055c2b
karllucas <karlkamakia@gmail.com>
2022-03-10 04:53:39 -0800

    Q4 first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	//string initialisation
+	char string[] = "Programming is like building a multilingual puzzle.";
+	puts(string); //Writes 'string' to stdout
+	return 0;
+}
