9f7345077c9da9ef07e7508e77675c9c06199754
karllucas <karlkamakia@gmail.com>
2022-03-10 16:00:28 -0800

    Q8 commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*Program initialization with main()
+ *perror prints string to standard eror
+ *return(1) returns 1 as the final output
+ *program terminates at closing }
+*/
+int main()
+{
+        perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return(1);
+}
