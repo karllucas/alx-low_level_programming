9dbdb140e01a916119375ee141769e5c2ef0df23
karllucas <karlkamakia@gmail.com>
2022-03-10 16:24:56 -0800

    Q7 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
-
-/*Program initialization with main()
- *perror prints string to standard eror
- *return(1) returns 1 as the final output
- *program terminates at closing }
-*/
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-        return(1);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
+	return (1);
+}
