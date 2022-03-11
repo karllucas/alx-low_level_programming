5ffa8f30224bceb789d88a237da4f6331350e487
karllucas <karlkamakia@gmail.com>
2022-03-10 16:16:57 -0800

    Q7 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,7 @@
  *return(1) returns 1 as the final output
  *program terminates at closing }
 */
-int main()
+int main(void)
 {
-        perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return(1);
-}
+perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+        return(1);
