2a5c800fe287865850acb50ac5acdb167d379fb1
karllucas <karlkamakia@gmail.com>
2022-03-15 18:56:23 -0700

    Q1 commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+
+	return(0);
+}
