1570df90528653f177a740f29a68167fd0195e53
karllucas <karlkamakia@gmail.com>
2022-03-17 05:08:21 -0700

    Q2 commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,13 @@
 /**
- * int mul(int a, int b) - multiplies int a and int b
- * Return: Always 0
+ * mul - multiplies two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: return results
 */
 
 int mul(int a, int b)
 {
-	int c;
-	c = a*b;
-
-	return (c);
+	return (a * b);
 }
