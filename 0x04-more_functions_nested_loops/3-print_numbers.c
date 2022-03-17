06e85f6358d5dcec5ce8c98051b67a4c108c0ebc
karllucas <karlkamakia@gmail.com>
2022-03-17 06:59:02 -0700

    Q3 commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,8 +7,21 @@
 void print_numbers(void)
 {
         int i;
-	
-	for (i >= 48 && i <= 57)
+
+        for (i = 48; i <= 57)
+        ^C
+root@82534809e2ff:~/alx-low_level_programming/0x04-more_functions_nested_loops# cat > 3-print_numbers.c
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers
+ *
+ */
+void print_numbers(void)
+{
+        int i;
+
+        for (i = 48; i <= 57; i++)
 	{
 		_putchar(i);
         }
