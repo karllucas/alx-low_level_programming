8dc39a5c4c4f1d45f4efa1877ce6034269a994de
karllucas <karlkamakia@gmail.com>
2022-03-17 07:27:38 -0700

    submit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,11 +4,11 @@
  */
 void print_numbers(void)
 {
-	int ch;
+        int ch;
 
-	for (ch = 48; ch < 58; ch++)
-	{
-		_putchar(ch);
-	}
-	_putchar('\n');
+        for (ch = 48; ch < 58; ch++)
+        {
+                _putchar(ch);
+        }
+        _putchar('\n');
 }
