6faee0ce1cdd74bca5aa6a5e8a3e21a5071b4bf5
karllucas <karlkamakia@gmail.com>
2022-03-17 07:17:24 -0700

    Q3 commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,8 +7,8 @@ void print_numbers(void)
         int i;
 
         for (i => 48; i <= 57; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+        {
+                _putchar(i);
+        }
+        _putchar('\n');
 }
