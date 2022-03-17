98b219b00008e67d2c4564e07f5195f7badcf29e
karllucas <karlkamakia@gmail.com>
2022-03-17 04:06:23 -0700

    Q0 commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -11,13 +11,12 @@
 
 int _isupper(int c)
 {
-        if (c >= 65 && c <= 90)
+        while (c >= 65 && c <= 90)
         {
-                return (1);
+                return(1);
         }
         else
         {
-                return (0);
+                return(0);
         }
-	c++;
 }
