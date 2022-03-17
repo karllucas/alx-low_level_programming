a18fdd36d90e2407641191d9177703a593f67d6e
karllucas <karlkamakia@gmail.com>
2022-03-17 01:25:13 -0700

    Q1 commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/* `isdigit` function checks for a digit from a given input
+ * returns 1 if input is a digit
+ * else returns 0
+*/
+
+int c;
+c = 0;
+
+int main(int c)
+{
+	return(c <= 9);
+	c++;
+}
