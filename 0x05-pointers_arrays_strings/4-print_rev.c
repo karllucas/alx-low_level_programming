24ea0f2d296578e371b9183d170e4e587755a49c
karllucas <karlkamakia@gmail.com>
2022-03-21 12:26:15 -0700

    Q4 commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,3 +6,21 @@
  */
 void print_rev(char *s)
 {
+	int i, j, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
