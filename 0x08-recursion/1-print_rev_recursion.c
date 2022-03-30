a66f355612ac0d0491b847425da0103b99c68f42
karllucas <karlkamakia@gmail.com>
2022-03-30 12:00:43 -0700

    Q1 commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse
- * @s: string to print
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: string
+ * Return: no return.
  */
 void _print_rev_recursion(char *s)
 {
-        if (*s >= 0)
-        {
-                _putchar(*s);
-                _puts_recursion(s - 1);
-        }
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
