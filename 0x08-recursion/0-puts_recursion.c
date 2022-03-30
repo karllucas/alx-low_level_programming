09b476b6a52c9f25d7ddac3ae2de81eec852d477
karllucas <karlkamakia@gmail.com>
2022-03-30 11:51:51 -0700

    QO commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: string
- * Return: no return.
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
  */
 void _puts_recursion(char *s)
 {
