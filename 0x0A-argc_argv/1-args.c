8be465495bd2a5c1de3b66de2e22f18e84512d32
karllucas <karlkamakia@gmail.com>
2022-04-01 04:58:43 -0700

    Q1 commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,8 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-        printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
