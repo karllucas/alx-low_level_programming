191adcc9ab23c66e915d5b25ce0550b51308661c
karllucas <karlkamakia@gmail.com>
2022-04-04 14:00:46 -0700

    Q0 commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,30 +4,31 @@
 /**
  * *create_array - creates an array of chars,
  * and initializes it with a specific char
- * @size: size of array to create
- * @c: char to initialize array of c
+ * @size: size of the array to create
+ * @c: char to initialize the array c
+ *
  * Return: pointer to the array (Success), NULL (Error)
  */
 char *create_array(unsigned int size, char c)
 {
-        char *p;
+	char *p;
 	unsigned int i = 0;
 
-        if (size == 0)
-                return(NULL);
+	if (size == 0)
+		return (NULL);
 
-        p = (char *) malloc(sizeof(char) * size);
+	p = (char *) malloc(sizeof(char) * size);
 
-        if (p == NULL)
-                return(0);
+	if (p == NULL)
+		return (0);
 
-        while (i < size)
-        {
-                *(p + i) = c;
-                i++;
-        }
+	while (i < size)
+	{
+		*(p + i) = c;
+		i++;
+	}
 
-        *(p + i) = '\0';
+	*(p + i) = '\0';
 
-        return(p);
+	return (p);
 }
