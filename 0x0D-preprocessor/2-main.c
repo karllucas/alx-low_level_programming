c8a376987fa258618d512bfc4dd3175b5b94caa3
karllucas <karlkamakia@gmail.com>
2022-04-11 13:07:43 -0700

    Q2 commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,12 @@
-#include <stdlib.h>
+#include <stdio.h>
 
+/**
+ * main - prints the name of the file.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
