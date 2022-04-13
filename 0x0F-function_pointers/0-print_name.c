71c94e6e9ef8cdfb79f6508dde0c5ca99392201e
karllucas <karlkamakia@gmail.com>
2022-04-13 08:13:07 -0700

    submit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -9,6 +10,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+        if (name && f)
+                f(name);
 }
