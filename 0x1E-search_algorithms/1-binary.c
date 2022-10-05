1417a62c1440a22b3d04ef47151f7bad93d5a023
Karl Kamakia <karlkamakia@gmail.com>
2022-10-06 01:01:55 +0300

    Update and rename binary_search.c to 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -7,18 +7,6 @@
  *
  * Return: Always EXIT_SUCCESS
  */
-int main(void)
-{
-    int array[] = {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-    };
-    size_t size = sizeof(array) / sizeof(array[0]);
-
-    printf("Found %d at index: %d\n\n", 2, binary_search(array, size, 2));
-    printf("Found %d at index: %d\n\n", 5, binary_search(array, 5, 5));
-    printf("Found %d at index: %d\n", 999, binary_search(array, size, 999));
-    return (EXIT_SUCCESS);
-}
 
 int binary_search(int *array, size_t size, int value)
 {
@@ -27,9 +15,10 @@ int binary_search(int *array, size_t size, int value)
     size_t md = lo + ((hi - lo) / 2);
 
     if (array == NULL || size == NULL)
+    {
         return (-1);
+    }
 
-    //Run loop to check if value is greater than midpoint
     do
     {
         if (array[i] == value)
